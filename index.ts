c97f224d1068bfaff9d09f241f7cc4abfd4bcc11
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-10-27 21:23:05 +0200

    Login/wait and screenshot routine

--- index.ts
+++ index.ts
@@ -1,4 +1,22 @@
 require('dotenv').config();
-const puppeteer = require('puppeteer');
+import * as puppeteer from 'puppeteer';
+import selectors from './selectors';
 
-console.log(`${process.env.BITBUCKET_USER}:${process.env.BITBUCKET_PASS}`);
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('https://bitbucket.org/account/signin/');
+  await page.type(selectors.EmailInput, process.env.BITBUCKET_EMAIL!);
+  await page.type(selectors.PasswordInput, process.env.BITBUCKET_PASS!);
+  await page.click(selectors.SubmitButton);
+  await page.waitForNavigation();
+  await page.waitForSelector(selectors.DashboardHeader);
+  const dashboardElement = await page.$(selectors.DashboardOverview);
+  const clip = await dashboardElement.boundingBox();
+  await page.screenshot({
+    path: 'screenshots/dashboard.png',
+    type: 'png',
+    clip: clip,
+  });
+  await page.close();
+})();
